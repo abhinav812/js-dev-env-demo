39bb4209780f430a4d036ba2c7c208e5d676c9fe
abhinav812 <abhinav_b4u@hotmail.com>
2017-07-10 22:36:47 +0530

    Production deployment to Heroku & Surge changes added.

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -14,14 +14,6 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
-app.get("/users", function(req, res) {
-  // Mocking some data
-  res.json([
-    {"id":1, "firstName": "Abhinav", "lastName": "Kumar", "email": "abhinav.kumar@gmail.com" },
-    {"id":2, "firstName": "Abhishek", "lastName": "Chandra", "email": "abhishek.chandra@gmail.com" }
-  ]);
-});
-
 app.listen(port, function (err) {
   if (err) {
     console.log(err);
